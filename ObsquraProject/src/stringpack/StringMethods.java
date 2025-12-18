7d6f3569c8d996496b676101e5a0366098aaee8c
USER <USER@LAPTOP-VJLTA5G6>
2025-12-18 10:47:10 +0400

    Java commit

---
+++ ObsquraProject/src/stringpack/StringMethods.java
@@ -0,0 +1,11 @@
+package stringpack;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello!!!!");
+
+	}
+
+}
